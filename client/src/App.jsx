86febe52b7f751620a469f4bad0ea875ceef7ad0
sarathkumar s <sarathkumar93622@gmail.com>
2025-09-14 22:34:33 +0530

    commit changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,18 @@
-
-import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './Components/Header'
-import Footer from './Components/Footer'
-import Projects from './pages/Projects'
-import Projectdetails from './pages/Projectdetails'
-import StartProject from './Pages/StartProject'
-import Login from './pages/Login'
-import AdminLogin from './pages/AdminLogin'
-import Signup from './pages/Signup'
-import { ProjectProvider } from './context/ProjectContext'
-import { AuthProvider } from './context/AuthContext'
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Projects from "./pages/Projects";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { ProjectProvider } from "./context/ProjectContext";
+import { AuthProvider } from "./context/AuthContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Home from './pages/Home'
-
+import Home from "./pages/Home";
+import ProjectDetails from "./pages/Projectdetails";
+import StartProject from "./Pages/StartProject";
+import AdminLogin from "./pages/AdminLogin";
 
 function App() {
   return (
@@ -27,8 +25,8 @@ function App() {
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/projects" element={<Projects />} />
-                <Route path="/project/:id" element={<Projectdetails />} />
-               <Route path="/start-project" element={<StartProject />} /> 
+                <Route path="/project/:id" element={<ProjectDetails />} />
+                <Route path="/start-project" element={<StartProject />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/admin-login" element={<AdminLogin />} />
                 <Route path="/signup" element={<Signup />} />
@@ -40,7 +38,7 @@ function App() {
         </Router>
       </ProjectProvider>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
