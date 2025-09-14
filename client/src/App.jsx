78a8d561ef0126d3262e27837e0a43cb3cbbcc82
sarathkumar s <sarathkumar93622@gmail.com>
2025-09-14 22:08:11 +0530

    changed components mistake

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 // App.jsx
 import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Projectdetails from './pages/Projectdetails'
