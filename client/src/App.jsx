bf08df675d95ca08d825ea7b8f1726e97e754329
sarathkumar s <sarathkumar93622@gmail.com>
2025-09-14 22:30:41 +0530

    commit changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,23 +3,17 @@ import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
-import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Projectdetails from './pages/Projectdetails'
 import StartProject from './Pages/StartProject'
 import Login from './pages/Login'
 import AdminLogin from './pages/AdminLogin'
 import Signup from './pages/Signup'
-import Feedback from './pages/Feedback'
-import BehaviorReport from './pages/BehaviorReport'
-import About from './pages/About'
-import Terms from './pages/Terms'
-import Privacy from './pages/Privacy'
-import Accessibility from './pages/Accessibility'
 import { ProjectProvider } from './context/ProjectContext'
 import { AuthProvider } from './context/AuthContext'
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Home from './pages/Home'
 
 
 function App() {
@@ -38,12 +32,6 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/admin-login" element={<AdminLogin />} />
                 <Route path="/signup" element={<Signup />} />
-                <Route path="/feedback" element={<Feedback />} />
-                <Route path="/behavior-report" element={<BehaviorReport />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/terms" element={<Terms />} />
-                <Route path="/privacy" element={<Privacy />} />
-                <Route path="/accessibility" element={<Accessibility />} />
               </Routes>
               <ToastContainer position="top-right" autoClose={3000} />
             </main>
